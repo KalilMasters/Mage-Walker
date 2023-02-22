c294e866a536106210bbef83e7cae402d9748eb3
mleung2@students.kennesaw.edu <>
2023-02-22 02:21:42 -0500

    Adjusted volume of jump in audacity

--- Assets/Scripts/Audio.cs
+++ Assets/Scripts/Audio.cs
@@ -18,7 +18,7 @@ public class Audio : MonoBehaviour
     }
     public void sound(AudioClip x)
     {
-        audioSrc.PlayOneShot(x, .15f);
+        audioSrc.PlayOneShot(x);
         Debug.Log("Played");
     }
 }
