10bc79abfa247de0a4494788e96a35505a0870a4
Gage18760@gmail.com <>
2023-04-03 20:56:01 -0400

    Button Sounds

--- Assets/Scripts/StartMenu/StartMenu.cs
+++ Assets/Scripts/StartMenu/StartMenu.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class StartMenu : MonoBehaviour
 {
+    public AudioClip button;
+    Audio adio;
     [SerializeField] GameObject[] Canvases;
     // Start is called before the first frame update
     void Start()
     {
+        adio = FindObjectOfType<Audio>();
         ResetCanvases();
         Canvases[0].SetActive(true);
     }
@@ -20,21 +23,25 @@ public class StartMenu : MonoBehaviour
     }
     public void ReturnButton()
     {
+        adio.sound(button);
         ResetCanvases();
         Canvases[0].SetActive(true);
     }
     public void PlayButton()
     {
+        adio.sound(button);
         ResetCanvases();
         Canvases[1].SetActive(true);
     }
     public void SetttingsButton()
     {
+        adio.sound(button);
         ResetCanvases();
         Canvases[2].SetActive(true);
     }
     public void PlayGame(bool IsHardMode)
     {
+        adio.sound(button);
         // Load scene with Hardcore mode off
         MapManager.IsHardMode = IsHardMode;
         SceneManager.LoadScene(SceneNames.GameplayScene.ToString());
