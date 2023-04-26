345a6e01fd4abe0ee85a853bbded49e0aa87cd1a
mleung2@students.kennesaw.edu <>
2023-04-25 20:49:42 -0400

    Modified visuals of chain lightning Fixed lilypad glitch

--- Assets/Scripts/Ability/Chain Lightning/LightningProjectile.cs
+++ Assets/Scripts/Ability/Chain Lightning/LightningProjectile.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class LightningProjectile : Projectile
 {
     [SerializeField] GameObject chainLightning;
-
+    [SerializeField] GameObject lightningEffect;
 
     protected override void OnCollision(Collider collision)
     {
         base.OnCollision(collision);
+        Instantiate(lightningEffect, collision.transform.position, Quaternion.identity);
         Instantiate(chainLightning, collision.transform.position, Quaternion.identity);
     }
 }
