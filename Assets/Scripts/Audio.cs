b7adfc44b307381e63f39cb2cd200a1467817ad6
Gage18760@gmail.com <>
2023-04-28 16:45:01 -0400

    Audio Settings and Pause Menu fixed

--- Assets/Scripts/Audio.cs
+++ Assets/Scripts/Audio.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
 
 public class Audio : MonoBehaviour
 {
     public AudioMixer mixer;
     public AudioSource audioSrc;
+    static float temp;
     
     // Start is called before the first frame update
     void Start()
@@ -17,12 +19,20 @@ public class Audio : MonoBehaviour
     public void sound(AudioClip x)
     {
         //Debug.Log("Played");
-        audioSrc.PlayOneShot(x, .15f);
+        audioSrc.PlayOneShot(x, temp);
     }
 
     public void audioLevel(float slider)
     {
         mixer.SetFloat("MusicVol", Mathf.Log10 (slider) * 20);
         mixer.SetFloat("Sfx", Mathf.Log10(slider) * 20);
+        if (SceneManager.GetActiveScene().name != "MichaelTest") 
+            temp = slider; 
+        if (SceneManager.GetActiveScene().name == "MichaelTest")
+        {
+            temp = slider;
+            mixer.SetFloat("MusicVol", Mathf.Log10(temp) * 20);
+            mixer.SetFloat("Sfx", Mathf.Log10(temp) * 20);
+        }
     }
 }
